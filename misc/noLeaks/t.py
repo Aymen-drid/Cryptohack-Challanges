131860a38a412eeb6c75a432ea42179efd623628
aymen <aymen@kali.aymen>
2024-08-27 20:46:53 +0100

    finished noLeaks

--- misc/noLeaks/t.py
+++ misc/noLeaks/t.py
@@ -11,17 +11,38 @@ def json_send(hsh):
     request = json.dumps(hsh).encode()
     r.sendline(request)
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=}{[]:;'<>,.?/|\\`~"
+chars=list(chars)
 ans=[]
-while True:
+
+for i in range(15000):
     to_send = {"msg": "request"}
     json_send(to_send)
     received=json_recv()
     if received == {'error': 'Leaky ciphertext'}:
-        print(received)
         continue
     tt=received['ciphertext']
+    print (tt)
+    print(i)
     ciphertext = base64.b64decode(tt)
-    print(ciphertext)
-# with open('outputff.txt', 'w') as file:
-#     for item in ans:
-#         file.write(f"{item}\n")
+    ans.append([chr(i) for i in ciphertext])
+final=[]
+temp=[]
+for i in range(20):
+    final.append([]);
+for j in range(20):
+    ex=''
+    for k in ans:
+        ex+=k[j]
+    res=list(ex)
+    s=''
+    
+    for i in chars:
+        
+        if   i not in res :
+            print(i)  
+            temp.append(i)
+    final[j].append(temp)
+    temp=[]
+with open('final.txt', 'w') as file:
+    for item in final:
+        file.write(f"{item}\n")
